cdb1e282cfbc99f74b75943281b2f789f777c093
Ian Martiny <ian.martiny@colorado.edu>
2021-03-09 10:55:11 -0700

    write updated file

--- cmd/parseresults/main.go
+++ cmd/parseresults/main.go
@@ -41,6 +41,21 @@ func getJSON(path string) []byte {
 	return jsonBytes
 }
 
+func writeJSON(path string, lookup []experiment.LookupResult) {
+	file, err := os.Create(path)
+	if err != nil {
+		errorLogger.Fatalf("Error truncating file: %s, %v\n", path, err)
+	}
+	defer file.Close()
+
+	jsonBytes, err := json.Marshal(&lookup)
+	if err != nil {
+		errorLogger.Fatalf("Error turning lookup into bytes: %v\n", err)
+	}
+
+	file.Write(jsonBytes)
+}
+
 func parseABuf(abuf string) map[string][]string {
 	resMap := make(map[string][]string)
 	resBytes, err := base64.StdEncoding.DecodeString(abuf)
@@ -81,10 +96,43 @@ func parseResult(mResult results.MeasurementResult) []map[string][]string {
 func getDomains(results []experiment.LookupResult) map[string]int {
 	res := make(map[string]int)
 	for i, result := range results {
-		res[result.Domain] = i
+		res[result.Domain] = i + 1
 	}
 
 	return res
+}
+
+func findResult(rr []experiment.MeasurementResult, res results.MeasurementResult) (experiment.MeasurementResult, int) {
+	for i, r := range rr {
+		if r.ProbeID == res.PrbID {
+			if !intContains(r.IDs, res.MsmID) {
+				r.IDs = append(r.IDs, res.MsmID)
+			}
+			return r, i
+		}
+	}
+
+	return experiment.MeasurementResult{}, -1
+}
+
+func intContains(arr []int, i int) bool {
+	for _, a := range arr {
+		if a == i {
+			return true
+		}
+	}
+
+	return false
+}
+
+func strContains(arr []string, i string) bool {
+	for _, a := range arr {
+		if a == i {
+			return true
+		}
+	}
+
+	return false
 }
 
 func main() {
@@ -117,25 +165,43 @@ func main() {
 		for _, answer := range answers {
 			for domain, ipSlice := range answer {
 				var measResult experiment.MeasurementResult
-				measResult.ID = res.MsmID
-				measResult.ProbeID = res.PrbID
-				fmt.Printf("%s:\t%s\n", domain, ipSlice)
+				measIndex := -1
+				infoLogger.Printf("%s:\t%s\n", domain, ipSlice)
 				if lookupDomains[domain] > 0 {
+					if len(lookup[lookupDomains[domain]-1].RipeResults) > 0 {
+						measResult, measIndex = findResult(lookup[lookupDomains[domain]-1].RipeResults, res)
+					}
+					if measIndex == -1 {
+						if !intContains(measResult.IDs, res.MsmID) {
+							measResult.IDs = append(measResult.IDs, res.MsmID)
+						}
+						measResult.ProbeID = res.PrbID
+					}
 					for _, ip := range ipSlice {
 						if strings.Index(ip, ".") != -1 {
-							measResult.V4 = append(measResult.V4, ip)
+							if !strContains(measResult.V4, ip) {
+								measResult.V4 = append(measResult.V4, ip)
+							}
 						} else if strings.Index(ip, ":") != -1 {
-							measResult.V6 = append(measResult.V6, ip)
+							if !strContains(measResult.V6, ip) {
+								measResult.V6 = append(measResult.V6, ip)
+							}
 						}
 					}
 				}
 				if len(measResult.V4) > 0 || len(measResult.V6) > 0 {
-					lookup[lookupDomains[domain]].RipeResults = append(
-						lookup[lookupDomains[domain]].RipeResults, measResult,
-					)
+					if measIndex == -1 {
+						lookup[lookupDomains[domain]-1].RipeResults = append(
+							lookup[lookupDomains[domain]-1].RipeResults, measResult,
+						)
+					} else {
+						lookup[lookupDomains[domain]-1].RipeResults[measIndex] =
+							measResult
+					}
 				}
 			}
 		}
 	}
 	fmt.Printf("%+v\n", lookup)
+	writeJSON(*jsonPath, lookup)
 }
