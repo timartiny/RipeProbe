caff39b565fcae6d76420fea794c7cdf316ddda5
Ian Martiny <ian.martiny@colorado.edu>
2021-04-02 15:57:01 -0600

    add sanity structure to data folder

--- fetchMeasurementResults.sh
+++ fetchMeasurementResults.sh
@@ -30,8 +30,14 @@ if [ ${#input} == 0 ]; then
     echo "Need file to get measurement IDs, use -f"
     exit 1
 fi
+firstMeasurement=$(head -n 1 $input)
+lastMeasurement=$(tail -n 1 $input)
+folderName="$firstMeasurement-$lastMeasurement"
+mkdir -p data/${folderName}
+echo "Authorization: Key $apiKey"
+echo "Writing measurement data to data/${folderName}/"
 while read -r line; do
-    echo "Authorization: Key $apiKey"
     echo "https://atlas.ripe.net/api/v2/measurements/$line/results/"
-    curl -H "Authorization: Key $apiKey" "https://atlas.ripe.net/api/v2/measurements/$line/results/" > data/${line}_results.json
+    curl -H "Authorization: Key $apiKey" "https://atlas.ripe.net/api/v2/measurements/$line/results/" > data/${folderName}/${line}_results.json
 done < "$input"
+echo "Wrote measurement data to data/${folderName}/"
