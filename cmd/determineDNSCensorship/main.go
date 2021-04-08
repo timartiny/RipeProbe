651543dcfeab3a32d51bdb30ab657e93079319ab
Ian Martiny <ian.martiny@colorado.edu>
2021-04-08 17:19:02 -0600

    parse name servers properly

--- cmd/determineDNSCensorship/main.go
+++ cmd/determineDNSCensorship/main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
+	"net/url"
 	"os"
 	"sort"
 	"strings"
@@ -62,7 +63,15 @@ func getProbes(r Results) Ints {
 }
 
 type IPs []net.IP
-type DomainToIPs map[string]IPs
+type IPPair struct {
+	V4 IPs
+	V6 IPs
+}
+type DomainToIPs map[string]IPPair
+type Resolvers struct {
+	Domains DomainToIPs
+	Open    IPPair
+}
 
 func (l IPs) Contains(n net.IP) bool {
 	for _, v := range l {
@@ -72,11 +81,6 @@ func (l IPs) Contains(n net.IP) bool {
 	}
 
 	return false
-}
-
-type ResolverStats struct {
-	Domains       DomainToIPs
-	OpenResolvers IPs
 }
 
 func resolverStatsFromProbe(
@@ -97,10 +101,19 @@ func resolverStatsFromProbe(
 				fmt.Printf("empty one: %s: %v\n", qr.ResolverType, qr.ResolverIP)
 				continue
 			}
-			toDC[qr.ResolverType] = append(
-				toDC[qr.ResolverType],
-				net.ParseIP(qr.ResolverIP),
-			)
+			ip := net.ParseIP(qr.ResolverIP)
+			if ip.To4() != nil {
+				toDC[qr.ResolverType] = IPPair{
+					V4: append(toDC[qr.ResolverType].V4, ip),
+					V6: toDC[qr.ResolverType].V6,
+				}
+			} else {
+				toDC[qr.ResolverType] = IPPair{
+					V4: toDC[qr.ResolverType].V4,
+					V6: append(toDC[qr.ResolverType].V6, ip),
+				}
+
+			}
 		}
 	}
 
@@ -108,22 +121,30 @@ func resolverStatsFromProbe(
 	rC <- toRC
 }
 
-func getResolverStats(fullResults Results) ResolverStats {
-	var ret ResolverStats
+func getResolverStats(fullResults Results) Resolvers {
+	var ret Resolvers
 	ret.Domains = make(DomainToIPs)
 	domainChan := make(chan DomainToIPs)
 	resolverChan := make(chan IPs)
 	var resolverWg sync.WaitGroup
-	domainIpCount := make(map[string]int)
-	resolverIpCount := make(map[string]int)
 
 	go func(domainChan <-chan DomainToIPs) {
 		for dtp := range domainChan {
 			for k, v := range dtp {
-				for _, ip := range v {
-					domainIpCount[ip.String()]++
-					if !ret.Domains[k].Contains(ip) {
-						ret.Domains[k] = append(ret.Domains[k], ip)
+				for _, ip := range v.V4 {
+					if !ret.Domains[k].V4.Contains(ip) {
+						ret.Domains[k] = IPPair{
+							V4: append(ret.Domains[k].V4, ip),
+							V6: ret.Domains[k].V6,
+						}
+					}
+				}
+				for _, ip := range v.V6 {
+					if !ret.Domains[k].V6.Contains(ip) {
+						ret.Domains[k] = IPPair{
+							V4: ret.Domains[k].V4,
+							V6: append(ret.Domains[k].V6, ip),
+						}
 					}
 				}
 			}
@@ -133,11 +154,15 @@ func getResolverStats(fullResults Results) ResolverStats {
 	go func(resolverChan <-chan IPs) {
 		for ips := range resolverChan {
 			for _, ip := range ips {
-				resolverIpCount[ip.String()]++
-				if !ret.OpenResolvers.Contains(ip) {
-					ret.OpenResolvers = append(ret.OpenResolvers, ip)
+				if ip.To4() != nil {
+					if !ret.Open.V4.Contains(ip) {
+						ret.Open.V4 = append(ret.Open.V4, ip)
+					}
+				} else {
+					if !ret.Open.V6.Contains(ip) {
+						ret.Open.V6 = append(ret.Open.V6, ip)
+					}
 				}
-
 			}
 		}
 	}(resolverChan)
@@ -148,8 +173,7 @@ func getResolverStats(fullResults Results) ResolverStats {
 			probeResults.V4ToV4,
 			domainChan,
 			resolverChan,
-			&resolverWg,
-		)
+			&resolverWg)
 		go resolverStatsFromProbe(
 			probeResults.V4ToV6,
 			domainChan,
@@ -174,16 +198,14 @@ func getResolverStats(fullResults Results) ResolverStats {
 
 	close(domainChan)
 	close(resolverChan)
-	infoLogger.Printf("domain ipCount: %v\n", domainIpCount)
-	infoLogger.Printf("resolver ipCount: %v\n", resolverIpCount)
 
 	return ret
 }
 
-type QueryStats []string
+type Queries []string
 
-func getQueryStats(fullResults Results) QueryStats {
-	var ret QueryStats
+func getQueryStats(fullResults Results) Queries {
+	var ret Queries
 
 	pr := fullResults[0]
 	queries := pr.V4ToV4[0].Queries
@@ -194,13 +216,14 @@ func getQueryStats(fullResults Results) QueryStats {
 	return ret
 }
 
-func printResolverStats(rs ResolverStats, printIPs bool) {
+func printResolverStats(rs Resolvers, printIPs bool) {
 	numDomainResolvers := len(rs.Domains)
 	numDomainIPs := 0
 	for _, ips := range rs.Domains {
-		numDomainIPs += len(ips)
+		numDomainIPs += len(ips.V4)
+		numDomainIPs += len(ips.V6)
 	}
-	numOpenResolvers := len(rs.OpenResolvers)
+	numOpenResolvers := len(rs.Open.V4) + len(rs.Open.V6)
 	numResolvers := numDomainResolvers + numOpenResolvers
 	fmt.Printf("Number of Resolvers: %d\n", numResolvers)
 	fmt.Printf(
@@ -208,75 +231,213 @@ func printResolverStats(rs ResolverStats, printIPs bool) {
 		numDomainResolvers,
 		numDomainIPs,
 	)
-	for d, ips := range rs.Domains {
-		var v4s []net.IP
-		var v6s []net.IP
-		for _, ip := range ips {
-			if ip.To4() != nil {
-				v4s = append(v4s, ip)
-			} else {
-				v6s = append(v6s, ip)
-			}
-		}
-		if printIPs {
-			fmt.Printf("\t\t%s:\t%d ips\n", d, len(ips))
-			fmt.Printf("\t\t\tV4 IPs: %d\n", len(v4s))
-			for _, ip := range v4s {
+	if printIPs {
+		for d, ips := range rs.Domains {
+			fmt.Printf("\t\t%s:\t%d ips\n", d, len(ips.V4)+len(ips.V6))
+			fmt.Printf("\t\t\tV4 IPs: %d\n", len(ips.V4))
+			for _, ip := range ips.V4 {
 				fmt.Printf("\t\t\t\t%s\n", ip.String())
 			}
-			fmt.Printf("\t\t\tV6 Ips: %d\n", len(v6s))
-			for _, ip := range v6s {
+			fmt.Printf("\t\t\tV6 Ips: %d\n", len(ips.V6))
+			for _, ip := range ips.V6 {
 				fmt.Printf("\t\t\t\t%s\n", ip.String())
 			}
 		}
 	}
 	fmt.Printf("\tNumber of Open Resolvers: %d\n", numOpenResolvers)
-	var openV4s []net.IP
-	var openV6s []net.IP
-	for _, ip := range rs.OpenResolvers {
-		if ip.To4() != nil {
-			openV4s = append(openV4s, ip)
-		} else {
-			openV6s = append(openV6s, ip)
-		}
-	}
 	if printIPs {
-		fmt.Printf("\t\t\tV4 IPs: %d\n", len(openV4s))
-		for _, ip := range openV4s {
+		fmt.Printf("\t\t\tV4 IPs: %d\n", len(rs.Open.V4))
+		for _, ip := range rs.Open.V4 {
 			fmt.Printf("\t\t\t\t%s\n", ip.String())
 		}
-		fmt.Printf("\t\t\tV6 IPs: %d\n", len(openV6s))
-		for _, ip := range openV6s {
+		fmt.Printf("\t\t\tV6 IPs: %d\n", len(rs.Open.V6))
+		for _, ip := range rs.Open.V6 {
 			fmt.Printf("\t\t\t\t%s\n", ip.String())
 		}
 	}
 }
 
-func printQueryStats(qs QueryStats) {
+func printQueryStats(qs Queries) {
 	fmt.Printf("Domains to be resolved: %d\n", len(qs))
 	for _, q := range qs {
 		fmt.Printf("\t%s\n", q)
 	}
 }
 
-func printGeneralStats(
-	np int,
-	rs ResolverStats,
-	printIPs bool,
-	qs QueryStats,
-) {
-	fmt.Printf("Number of probes in this measurement: %d\n", np)
-	printResolverStats(rs, printIPs)
-	printQueryStats(qs)
+func printGeneralStats(gS GenStats, printIPs bool) {
+	fmt.Printf("Number of probes in this measurement: %d\n", gS.NumProbes)
+	printResolverStats(gS.ResolverStats, printIPs)
+	printQueryStats(gS.QueryStats)
 }
 
-func generalStats(fullResults Results, printIPs bool, wg *sync.WaitGroup) {
-	defer wg.Done()
-	numProbes := getNumProbes(fullResults)
-	resolverStats := getResolverStats(fullResults)
-	queryStats := getQueryStats(fullResults)
+func generalStats(fullResults Results, genChan chan<- GenStats) {
+	var ret GenStats
+	ret.NumProbes = getNumProbes(fullResults)
+	ret.ResolverStats = getResolverStats(fullResults)
+	ret.QueryStats = getQueryStats(fullResults)
 
-	printGeneralStats(numProbes, resolverStats, printIPs, queryStats)
+	genChan <- ret
+}
+
+type GenStats struct {
+	NumProbes     int
+	ResolverStats Resolvers
+	QueryStats    Queries
+}
+
+func printSpecificResults(stats SpecificResults) {
+	for dom, spDom := range stats {
+		fmt.Printf("\tfor %s:\n", dom)
+		fmt.Printf("\t\tOpen Resolvers responded:\n")
+		if spDom.Open.Addresses > 0 {
+			fmt.Printf("\t\t\tWith Addresses: %d\n", spDom.Open.Addresses)
+		}
+		if spDom.Open.Timeouts > 0 {
+			fmt.Printf("\t\t\tWith Timeouts: %d\n", spDom.Open.Timeouts)
+		}
+		if spDom.Open.NameServers > 0 {
+			fmt.Printf("\t\t\tWith NameServers: %d\n", spDom.Open.NameServers)
+		}
+		if spDom.Open.Other > 0 {
+			fmt.Printf("\t\t\tWith Other: %d\n", spDom.Open.Other)
+			fmt.Printf("\t\t\t\tSee: \n")
+			for _, in := range spDom.Domain.OtherInfo {
+				fmt.Printf(
+					"\t\t\t\t\t{ProbeID: %d, Target: %s}\n",
+					in.ProbeID,
+					in.IP.String(),
+				)
+			}
+		}
+
+		fmt.Printf("\t\tDomain IPs responded:\n")
+		if spDom.Domain.Addresses > 0 {
+			fmt.Printf("\t\t\tWith Addresses: %d\n", spDom.Domain.Addresses)
+		}
+		if spDom.Domain.Timeouts > 0 {
+			fmt.Printf("\t\t\tWith Timeouts: %d\n", spDom.Domain.Timeouts)
+		}
+		if spDom.Domain.NameServers > 0 {
+			fmt.Printf("\t\t\tWith NameServers: %d\n", spDom.Domain.NameServers)
+		}
+		if spDom.Domain.Other > 0 {
+			fmt.Printf("\t\t\tWith Other: %d\n", spDom.Domain.Other)
+			fmt.Printf("\t\t\t\tSee: \n")
+			for _, in := range spDom.Domain.OtherInfo {
+				fmt.Printf(
+					"\t\t\t\t\t{ProbeID: %d, Target: %s}\n",
+					in.ProbeID,
+					in.IP.String(),
+				)
+			}
+		}
+
+	}
+}
+
+func printStats(genChan <-chan GenStats,
+	v4AChan <-chan SpecificResults,
+	wg *sync.WaitGroup,
+	printIPs bool,
+) {
+	gS := <-genChan
+	printGeneralStats(gS, printIPs)
+	wg.Done()
+
+	v4AStats := <-v4AChan
+	fmt.Printf("When a V4 address was asked to resolve a domain for an A record:\n")
+	printSpecificResults(v4AStats)
+	wg.Done()
+}
+
+type Info struct {
+	ProbeID int
+	IP      net.IP
+}
+type Infos []Info
+type SpecificResponse struct {
+	Addresses   int
+	Timeouts    int
+	NameServers int
+	Other       int
+	OtherInfo   Infos
+}
+type SpecificDomain struct {
+	Open   SpecificResponse
+	Domain SpecificResponse
+}
+type SpecificResults map[string]SpecificDomain
+
+func update(sr, newSr SpecificResponse) SpecificResponse {
+	sr.Addresses += newSr.Addresses
+	sr.Timeouts += newSr.Timeouts
+	sr.NameServers += newSr.NameServers
+	sr.Other += newSr.Other
+	sr.OtherInfo = append(sr.OtherInfo, newSr.OtherInfo...)
+	return sr
+}
+
+func isUrl(str string) bool {
+	// _, err := url.ParseRequestURI(str)
+	// if err != nil {
+	// 	return false
+	// }
+	_, err := url.Parse(str)
+	return err == nil
+}
+
+func getSpecificResponse(strs []string, prID int, resIP string) SpecificResponse {
+	info := Info{ProbeID: prID, IP: net.ParseIP(resIP)}
+	var sr SpecificResponse
+	for _, str := range strs {
+		if net.ParseIP(str) != nil {
+			sr.Addresses++
+		} else if strings.Split(str, ": ")[0] == "timeout" {
+			sr.Timeouts++
+		} else if len(str) == 0 {
+			infoLogger.Printf("len of str is 0\n")
+		} else if isUrl(str) {
+			sr.NameServers++
+		} else {
+			sr.Other++
+			sr.OtherInfo = append(sr.OtherInfo, info)
+		}
+	}
+
+	return sr
+}
+
+func v4AStats(fR Results, v4AChan chan<- SpecificResults) {
+	toChan := make(SpecificResults)
+
+	for _, pr := range fR {
+		for _, qr := range pr.V4ToV4 {
+			for dom, resps := range qr.Queries {
+				if _, ok := toChan[dom]; !ok {
+					toChan[dom] = SpecificDomain{
+						Open:   SpecificResponse{},
+						Domain: SpecificResponse{},
+					}
+				}
+				if strings.Contains(qr.ResolverType, "_Resolver") {
+					resp := getSpecificResponse(resps, pr.ProbeID, qr.ResolverIP)
+					toChan[dom] = SpecificDomain{
+						Open:   update(toChan[dom].Open, resp),
+						Domain: toChan[dom].Domain,
+					}
+				} else {
+					resp := getSpecificResponse(resps, pr.ProbeID, qr.ResolverIP)
+					toChan[dom] = SpecificDomain{
+						Open:   toChan[dom].Open,
+						Domain: update(toChan[dom].Domain, resp),
+					}
+				}
+			}
+
+		}
+	}
+
+	v4AChan <- toChan
 }
 
 func main() {
@@ -294,11 +455,17 @@ func main() {
 		log.Ldate|log.Ltime|log.Lshortfile,
 	)
 	var wg sync.WaitGroup
+	genChan := make(chan GenStats)
+	v4AChan := make(chan SpecificResults)
 	fullResults := getStruct(*resultsPath)
 
+	go printStats(genChan, v4AChan, &wg, *printIPs)
+
 	wg.Add(1)
-	go generalStats(fullResults, *printIPs, &wg)
+	go generalStats(fullResults, genChan)
+
+	wg.Add(1)
+	go v4AStats(fullResults, v4AChan)
 
 	wg.Wait()
-
 }
