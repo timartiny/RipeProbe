fbf3f343c180936d9aa5aab08d8d02a788612c89
Ian Martiny <ian.martiny@colorado.edu>
2021-09-23 16:10:52 -0600

    remove unused code

--- ripeexperiment/dnslookup.go
+++ ripeexperiment/dnslookup.go
@@ -1,14 +1,9 @@
 package ripeexperiment
 
 import (
-	"encoding/csv"
 	"encoding/json"
-	"io"
-	"net"
 	"os"
-	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	atlas "github.com/keltia/ripe-atlas"
@@ -124,118 +119,4 @@ func LookupAtlas(queries []string, apiKey string, probeIds []string, targets []s
 	)
 
 	return resp.Measurements, nil
-}
-
-func lookup(record []string, data chan LookupResult, wg *sync.WaitGroup) {
-	ipRecords, _ := net.LookupIP(record[1])
-	rank, err := strconv.Atoi(record[0])
-	if err != nil {
-		// errorLogger.Printf(
-		// 	"Error converting rank to int: %v, %v\n",
-		// 	record[0],
-		// 	err,
-		// )
-		rank = -1
-	}
-	result := LookupResult{
-		Domain: record[1],
-		Rank:   rank,
-		Source: record[2],
-	}
-	for _, ip := range ipRecords {
-		// fmt.Println(ip)
-		if ip.To4() == nil {
-			result.LocalV6 = append(result.LocalV6, ip.String())
-		} else {
-			result.LocalV4 = append(result.LocalV4, ip.String())
-		}
-	}
-
-	data <- result
-	wg.Done()
-}
-
-func writeDomain(data chan LookupResult, done chan bool, outPath string) {
-	f, err := os.Create(outPath)
-	if err != nil {
-		done <- false
-		errorLogger.Fatalf("can't open file, err: %v\n", err)
-	}
-	defer f.Close()
-
-	f.WriteString("[")
-	writeComma := false
-	for domain := range data {
-		if len(domain.LocalV4) == 0 || len(domain.LocalV6) == 0 {
-			continue
-		}
-		if writeComma {
-			f.WriteString(",")
-		}
-		jBytes, err := json.Marshal(&domain)
-		if err != nil {
-			errorLogger.Printf(
-				"Error converting data to json: %v, %v\n",
-				domain,
-				err,
-			)
-		}
-		f.Write(jBytes)
-		writeComma = true
-	}
-
-	f.WriteString("]")
-	done <- true
-}
-
-// LookupCSV reads domains from file and finds the ipaddr for each domain,
-// records v6 ones.
-func LookupCSV(csvPath, outPath string) {
-	csvFile, err := os.Open(csvPath)
-	if err != nil {
-		errorLogger.Fatalf(
-			"Could not open CSV file: %v, err: %v\n",
-			csvPath,
-			err,
-		)
-	}
-	defer csvFile.Close()
-
-	csvReader := csv.NewReader(csvFile)
-	wg := sync.WaitGroup{}
-	data := make(chan LookupResult)
-	done := make(chan bool)
-
-	//read title record
-	_, err = csvReader.Read()
-	if err != nil {
-		errorLogger.Fatalf("error reading record, err: %v\n", err)
-	}
-	for {
-		record, err := csvReader.Read()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			errorLogger.Fatalf("error reading record, err: %v\n", err)
-		}
-
-		// fmt.Printf("record: %v\n", record[2])
-		wg.Add(1)
-		go lookup(record, data, &wg)
-	}
-
-	go writeDomain(data, done, outPath)
-
-	go func() {
-		wg.Wait()
-		close(data)
-	}()
-
-	d := <-done
-
-	if d {
-		infoLogger.Printf("Wrote to %s successfully", outPath)
-	} else {
-		infoLogger.Println("Failed at writing to file")
-	}
 }
