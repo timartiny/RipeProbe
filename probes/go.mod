7b537c269738b1f89cc65a61486f55ca4141375c
Ian Martiny <ian.martiny@colorado.edu>
2021-09-24 12:54:22 -0600

    update probes to have a creation func, change name to SimpleProbe

--- probes/go.mod
+++ probes/go.mod
@@ -1,3 +1,5 @@
 module github.com/timartiny/RipeProbe/probes
 
 go 1.16
+
+require github.com/keltia/ripe-atlas v0.0.0-20210506215806-13f0d38c56e7 // indirect
--- probes/probes.go
+++ probes/probes.go
@@ -1,11 +1,34 @@
 package probes
 
+import atlas "github.com/keltia/ripe-atlas"
+
 // ProbeIP struct will store information to let probes be usable easily
-type ProbeIP struct {
+type SimpleProbe struct {
 	ID          int    `json:"id"`
 	AddressV4   string `json:"address_v4"`
 	PrefixV4    string `json:"prefix_v4"`
 	AddressV6   string `json:"address_v6"`
 	PrefixV6    string `json:"prefix_v6"`
 	CountryCode string `json:"country_code"`
+}
+
+func AtlasProbeToSimpleProbe(probe atlas.Probe) SimpleProbe {
+	var ret SimpleProbe
+	ret.ID = probe.ID
+	ret.AddressV4 = probe.AddressV4
+	ret.PrefixV4 = probe.PrefixV4
+	ret.AddressV6 = probe.AddressV6
+	ret.PrefixV6 = probe.PrefixV6
+	ret.CountryCode = probe.CountryCode
+
+	return ret
+}
+
+func AtlasProbeSliceToSimpleProbeSlice(probeSlice []atlas.Probe) []SimpleProbe {
+	var ret []SimpleProbe
+	for _, probe := range probeSlice {
+		ret = append(ret, AtlasProbeToSimpleProbe(probe))
+	}
+
+	return ret
 }
